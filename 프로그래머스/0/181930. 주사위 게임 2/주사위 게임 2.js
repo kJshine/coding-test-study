9e0924386e5266f4efbc734da7d94ea502219af1
devJaehwan <111832790+kJshine@users.noreply.github.com>
2025-01-07 10:54:52 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,15 @@
+function solution(a, b, c) {
+    var answer = 0;
+    // 1. 세 숫자가 모두 다르다 : a !== b && a !== c && b !== c
+    // 2. 두 숫자는 같고, 나머지가 다르다 a,b / a,c / b,c
+    // a === b && b !== c / a === c && a !== b / b === c && a !== b
+    // 3. 세 숫자가 모두 같다 a === b && b === c
+    if (a === b && b === c) {
+        answer = (a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2)) * (Math.pow(a, 3) + Math.pow                  (b, 3) + Math.pow(c, 3));
+    } else if (a !== b && a !== c && b !== c) {
+        answer = a + b + c;
+    } else {
+        answer = (a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
+    }
+    return answer;
+}
