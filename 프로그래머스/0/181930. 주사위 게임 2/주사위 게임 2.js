c4037b6575b34dd09660fe34e4956dca93ea5827
김재환 <fodore12@gmail.com>
2025-01-07 11:03:04 +0900

    md 작성

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -11,5 +11,9 @@ function solution(a, b, c) {
     } else {
         answer = (a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
     }
+    
+    // 조건문을 조금 더 간소화 할 수 있을까?
+    // 세 숫자가 모두 같다 => Math.max(a, b, c) === Math.min(a, b, c) : 간소화 되지는 않음
+    // 세 숫자가 모두 다르다, 두 숫자가 같고 하나만 다르다 => ??? 다른 방법이 생각나지 않음
     return answer;
 }
