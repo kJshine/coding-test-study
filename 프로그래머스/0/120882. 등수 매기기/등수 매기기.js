09ee0cbeaed583e6dcb05b263cfebf9fc3927880
devJaehwan <111832790+kJshine@users.noreply.github.com>
2025-04-01 10:13:02 +0900

    [level 0] Title: 등수 매기기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
@@ -0,0 +1,28 @@
+function solution(score) {
+    let answer = Array(score.length).fill(1);
+    
+    const scores = score.reduce((acc, cur, i) => {
+        const obj = {};
+        obj[(cur[0] + cur[1]) / 2] = i;
+        acc.push(obj);
+        return acc;
+    }, []).sort((a, b) => Object.keys(b) - Object.keys(a));
+    
+    let ranking = 0;
+    let prevScore = 0;
+    let equalScore = 0;
+    
+    scores.forEach((obj, i) => {
+        const key = Object.keys(obj);
+        if (+key !== prevScore) {
+            ranking += 1 + equalScore;
+            equalScore = 0;
+        } else {
+            equalScore++;
+        }
+        answer[obj[key]] = ranking;
+        prevScore = +key;
+    })
+    
+    return answer;
+}
