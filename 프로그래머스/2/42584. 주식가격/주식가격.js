a6d3a16176b48008bbab07852db11af55e588a98
devJaehwan <111832790+kJshine@users.noreply.github.com>
2025-06-12 14:14:12 +0900

    [level 2] Title: 주식가격, Time: 28.22 ms, Memory: 41.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,15 +1,33 @@
 function solution(prices) {
-    var answer = [];
+//     이중 반복문
+//     var answer = [];
+    
+//     for (let i = 0; i < prices.length; i++) {
+//         let second = 0;
+//         for (let j = i + 1; j < prices.length; j++) {
+//             second++;
+//             if (prices[i] > prices[j]) {
+//                 break;
+//             }
+//         }
+//         answer.push(second);
+//     }
+    
+    // 스택/큐
+    let answer = Array(prices.length).fill(0);
+    const stack = [];
     
     for (let i = 0; i < prices.length; i++) {
-        let second = 0;
-        for (let j = i + 1; j < prices.length; j++) {
-            second++;
-            if (prices[i] > prices[j]) {
-                break;
-            }
+        while (stack.length > 0 && prices[stack[stack.length - 1]] > prices[i]) {
+            const topIndex = stack.pop();
+            answer[topIndex] = i - topIndex;
         }
-        answer.push(second);
+        stack.push(i);
+    }
+    
+    while (stack.length > 0) {
+        const topIndex = stack.pop();
+        answer[topIndex] = prices.length - 1 - topIndex;
     }
     
     return answer;
